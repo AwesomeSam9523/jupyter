f789d409b33f7f95220b48ad811defe1b80887bb
Samaksh Gupta <samakshgupta04@gmail.com>
2021-02-18 21:15:44 +0530

    added chromedriver support try 1

--- DIKE_Bot.py
+++ DIKE_Bot.py
@@ -271,8 +271,13 @@ fnt3 = ImageFont.truetype('./fonts/Acme-Regular.ttf', 16)
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
-
-driver = webdriver.Chrome(executable_path=r"chromedriver.exe")
+GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.binary_location = GOOGLE_CHROME_PATH
+driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 driver.get('https://google.com')
 file = open('cookies.txt', 'r')
 cook = list(eval(file.read()))
